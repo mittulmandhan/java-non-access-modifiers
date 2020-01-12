82ed16f88035876a1ac75b2e4a89c2bcf6b30e42
mittulmandhan <conanmittul@gmail.com>
2020-01-12 22:07:26 +0530

    final keyword + definition of access and non-access modifiers

---
+++ src/main/java/com/finaldemo/A.java
@@ -0,0 +1,7 @@
+package com.finaldemo;
+// There are 2 types modifiers:
+// Access modifiers specifies how we can access the resources.
+// Non-access modifiers changes the behaviour of the resources.
+public class A {
+
+}
